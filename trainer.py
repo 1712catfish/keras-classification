ad20ca53fc8a7d5d4ea93bb4cee4b5ae5432a8da
tan-kun <1712huynhtan@gmail.com>
2023-02-24 11:41:40 +0700

    First commit

--- trainer.py
+++ trainer.py
@@ -1,22 +1,22 @@
 from utils import list_valid_args, retrieve_global_variables
 
 
-def train():
+def train(model, data_generator):
     k_fold_history = []
-    for d in DATA_GENERATOR:
+    for d in data_generator:
         print(f"========== Fold {d['index']} ==========")
 
         global_settings = retrieve_global_variables()
 
         args_dict = dict()
-        for k in set(list_valid_args(MODEL.fit)).intersection((set(global_settings).union(set(d.keys())))):
+        for k in set(list_valid_args(model.fit)).intersection((set(global_settings).union(set(d.keys())))):
             args_dict[k] = global_settings.get(k, d.get(k, None))
 
         print(1)
 
-        history = MODEL.fit(**args_dict)
+        history = model.fit(**args_dict)
 
-        MODEL.save_weights(f"model_{d['index']}.h5")
+        model.save_weights(f"model_{d['index']}.h5")
         k_fold_history.append(history.history)
 
-    return k_fold_history
+    return model, k_fold_history
